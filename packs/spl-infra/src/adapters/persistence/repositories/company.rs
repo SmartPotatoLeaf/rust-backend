7d1bb897fca7dcd709b2159b82c5e6e85826fbd0
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-02-11 22:56:16 -0500

    feat(infra): add company repository

---
+++ packs/spl-infra/src/adapters/persistence/repositories/company.rs
@@ -0,0 +1,39 @@
+use crate::adapters::persistence::entities::company;
+use sea_orm::*;
+use spl_domain::entities::company::Company;
+use spl_domain::ports::repositories::company::CompanyRepository;
+use spl_domain::ports::repositories::crud::CrudRepository;
+use spl_shared::adapters::persistence::repository::crud;
+use spl_shared::error::Result;
+use uuid::Uuid;
+
+pub struct DbCompanyRepository {
+    db: DatabaseConnection,
+}
+
+impl DbCompanyRepository {
+    pub fn new(db: DatabaseConnection) -> Self {
+        Self { db }
+    }
+}
+
+#[async_trait::async_trait]
+impl CrudRepository<Company, Uuid> for DbCompanyRepository {
+    async fn get_by_id(&self, id: Uuid) -> Result<Option<Company>> {
+        crud::get_by_id::<company::Entity, Company, Uuid>(&self.db, id).await
+    }
+
+    async fn create(&self, entity: Company) -> Result<Company> {
+        crud::create::<company::Entity, Company>(&self.db, entity).await
+    }
+
+    async fn update(&self, entity: Company) -> Result<Company> {
+        crud::update::<company::Entity, Company>(&self.db, entity).await
+    }
+
+    async fn delete(&self, id: Uuid) -> Result<Company> {
+        crud::delete::<company::Entity, Company, Uuid>(&self.db, id).await
+    }
+}
+
+impl CompanyRepository for DbCompanyRepository {}
