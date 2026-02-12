d5f6a4ff7c6fa04722b0ddb3f089b8781b365c6e
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-02-11 22:45:03 -0500

    feat(shared): add telemetry and logging infrastructure

---
+++ packs/spl-shared/src/telemetry.rs
@@ -0,0 +1,11 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+pub fn init_telemetry() {
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info,spl_backend=debug"));
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
