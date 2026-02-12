f784b04839d9a6280d5edb5b78db9073a61e3556
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-02-11 22:55:08 -0500

    feat(infra): add persistence entities module

---
+++ packs/spl-infra/src/adapters/persistence/entities/mod.rs
@@ -0,0 +1,7 @@
+pub mod company;
+pub mod diagnostics;
+pub mod feedback;
+pub mod image;
+pub mod plot;
+pub mod recommendation;
+pub mod user;
---
+++ packs/spl-infra/src/adapters/persistence/mod.rs
@@ -0,0 +1,3 @@
+pub mod entities;
+pub mod mappers;
+pub mod repositories;
