73dba1a3d88370359addcd96bc2a5af616d728ab
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-02-11 22:57:50 -0500

    feat(infra): add company web mapper

---
+++ packs/spl-infra/src/adapters/web/mappers/company.rs
@@ -0,0 +1,44 @@
+use crate::adapters::web::models::company::{
+    CompanyResponse, CreateCompanyRequest, SimplifiedCompanyResponse, UpdateCompanyRequest,
+};
+use spl_application::dtos::company::{CreateCompanyDto, UpdateCompanyDto};
+use spl_domain::entities::company::Company;
+
+impl From<CreateCompanyRequest> for CreateCompanyDto {
+    fn from(req: CreateCompanyRequest) -> Self {
+        Self {
+            name: req.name,
+            description: req.description,
+        }
+    }
+}
+
+impl From<UpdateCompanyRequest> for UpdateCompanyDto {
+    fn from(req: UpdateCompanyRequest) -> Self {
+        Self {
+            name: req.name,
+            description: req.description,
+        }
+    }
+}
+
+impl From<Company> for CompanyResponse {
+    fn from(company: Company) -> Self {
+        Self {
+            id: company.id,
+            name: company.name,
+            description: company.description,
+            created_at: company.created_at.to_rfc3339(),
+            updated_at: company.updated_at.to_rfc3339(),
+        }
+    }
+}
+
+impl From<Company> for SimplifiedCompanyResponse {
+    fn from(company: Company) -> Self {
+        Self {
+            id: company.id,
+            name: company.name,
+        }
+    }
+}
